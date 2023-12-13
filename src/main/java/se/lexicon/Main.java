25d86a3f944aaadc1c4639e3d4dc67b266c8d6b9
Goran Muminovic <goranmuminovic@gmail.com>
2023-12-13 12:50:05 +0100

    Changes by Jackie

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,7 +1,33 @@
 package se.lexicon;
 
+import se.lexicon.model.InterfaceProduct;
+import se.lexicon.model.VendingMachine;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    Drink coke = new Drink("Coca Cola","Small",200);
+//        System.out.println(coke.getId());
+        Drink pepsi = new Drink("Pepsi","Large",100);
+//        System.out.println(pepsi.getId());
+//        coke.examine();
+//        coke.use();
+
+
+        VendingMachineImpl Impl = new VendingMachineImpl();
+        Impl.addProduct(coke);
+        Impl.addProduct(pepsi);
+        Impl.addCurrency(100);
+
+
+        System.out.println(Impl.getBallance());
+        InterfaceProduct cc = Impl.request(coke.getId());
+
+
+        cc.examine();
+
+//        String productInformation = Impl.getProducts();
+
+//        System.out.println(productInformation);
     }
 }
