6a267fffaffffe4b302a3870b5dfb4263b2b7ed3
Goran Muminovic <goranmuminovic@gmail.com>
2024-05-02 13:50:21 +0200

    saving

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -10,21 +10,27 @@ public class Main {
 //        System.out.println(coke.getId());
         Drink pepsi = new Drink("Pepsi","Large",100);
 //        System.out.println(pepsi.getId());
-//        coke.examine();
-//        coke.use();
+  //      coke.examine();
+    //    coke.use();
 
 
         VendingMachineImpl Impl = new VendingMachineImpl();
         Impl.addProduct(coke);
         Impl.addProduct(pepsi);
-        Impl.addCurrency(100);
+        Impl.addCurrency(200);
+        Impl.addCurrency(200);
+        Impl.addProduct(coke);
 
 
         System.out.println(Impl.getBallance());
         InterfaceProduct cc = Impl.request(coke.getId());
+ //       System.out.println(Impl.getBallance());
 
 
         cc.examine();
+        Impl.endSession();
+        System.out.println(Impl.getBallance());
+
 
 //        String productInformation = Impl.getProducts();
 
